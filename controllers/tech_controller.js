684f5fb1e7721d873f1c4194ad16db2752cf6080
jdurant0529 <james.durant@gmail.com>
2016-07-18 19:13:58 -0400

    edit profile populated.

--- controllers/tech_controller.js
+++ controllers/tech_controller.js
@@ -1,4 +1,3 @@
-
 // Require orm
 var orm = require('../config/orm.js');
 
@@ -12,22 +11,22 @@ var app = express();
 module.exports = function(app, passport) {
 
 
-	// =====================================
-	// LOGIN ===============================
-	// =====================================
-	// show the login form
-	app.get('/', function(req, res) {
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/', function(req, res) {
 
-		// render the page and pass in any flash data if it exists
-		res.render('index', { message: req.flash('loginMessage'), background: true });
-	});
+        // render the page and pass in any flash data if it exists
+        res.render('index', { message: req.flash('loginMessage'), background: true });
+    });
 
-	// process the login form
-	app.post('/', passport.authenticate('local-login', {
-            successRedirect : '/employee_profile', // redirect to the secure profile section
-            failureRedirect : '/', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}),
+    // process the login form
+    app.post('/', passport.authenticate('local-login', {
+            successRedirect: '/employee_profile', // redirect to the secure profile section
+            failureRedirect: '/', // redirect back to the signup page if there is an error
+            failureFlash: true // allow flash messages
+        }),
         function(req, res) {
             console.log("hello");
 
@@ -70,7 +69,7 @@ module.exports = function(app, passport) {
         })
 
 
-    });  // end of get profile...
+    }); // end of get profile...
 
     // =====================================
     // LOGOUT ==============================
@@ -81,92 +80,97 @@ module.exports = function(app, passport) {
     });
 
 
-	// =====================================
-	// SIGNUP ==============================
-	// =====================================
-	// show the signup form
-	app.get('/signup', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('signup', { message: req.flash('signupMessage') });
-	});
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('signup', { message: req.flash('signupMessage') });
+    });
 
-	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/employee_profile', // redirect to the secure profile section
-		failureRedirect : '/signup', // redirect back to the signup page if there is an error
-		failureFlash : true // allow flash messages	
-	}));
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/employee_profile', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages	
+    }));
 
-	// =====================================
-	// PROFILE SECTION =========================
-	// =====================================
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/employee_profile', isLoggedIn, function(req, res) {
-		var userid = req.user.id;
-        orm.getPersonalData('users', userid, function(data){
+    // =====================================
+    // PROFILE SECTION =========================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/employee_profile', isLoggedIn, function(req, res) {
+        var userid = req.user.id;
+        orm.getPersonalData('users', userid, function(data) {
             console.log(data);
             res.render('employee/employee_profile', { user: data })
-             // get the user out of session and pass to template
+                // get the user out of session and pass to template
         });
-		// res.render('employee/employee_profile', {
-		// 	user : req.user // get the user out of session and pass to template
-		// });
-	});
+        // res.render('employee/employee_profile', {
+        // 	user : req.user // get the user out of session and pass to template
+        // });
+    });
 
-	// =====================================
-	// LOGOUT ==============================
-	// =====================================
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
-
-
-	// =====================================
-	// Employee ==============================
-	// =====================================
-	// app.get('/profile', function(req, res) {
-	// 	// load the edit_profile file
-	// 	res.render('profile'); 
-	// });
-	// app.get('/employee_profile', function(req, res) {
-	// 	// load the edit_profile file
-	// 	res.render('employee/employee_profile'); 
-	// });
-	app.get('/employee_edit_profile', function(req, res) {
-		// load the edit_profile file
-		res.render('employee/employee_edit_profile'); 
-	});
-	app.get('/employee_edit_resume', function(req, res) {
-		// load the edit_profile file
-		res.render('employee/employee_edit_resume'); 
-	});
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
 
-	// =====================================
-	// Employeer ==============================
-	// =====================================
-	app.get('/employeer', function(req, res) {
-		// load the edit_profile file
-		res.render('employeer/employeer'); 
-	});
-	app.get('/employeer_search', function(req, res) {
-		// load the edit_profile file
-		res.render('employeer/employeer_search'); 
-	});
-	app.get('/employeer_edit_contact', function(req, res) {
-		// load the edit_profile file
-		res.render('employeer/employeer_edit_contact'); 
-	});
-	app.get('/employeer_add_test', function(req, res) {
-		// load the edit_profile file
-		res.render('employeer/employeer_add_test'); 
-	});
-	app.get('/employeer_edit_test', function(req, res) {
-		// load the edit_profile file
-		res.render('employeer/employeer_edit_test'); 
-	});
+    // =====================================
+    // Employee ==============================
+    // =====================================
+    // app.get('/profile', function(req, res) {
+    // 	// load the edit_profile file
+    // 	res.render('profile'); 
+    // });
+    // app.get('/employee_profile', function(req, res) {
+    // 	// load the edit_profile file
+    // 	res.render('employee/employee_profile'); 
+    // });
+    app.get('/employee_edit_profile', function(req, res) {
+        // load the edit_profile file
+        console.log(req);
+        var userid = req.user.id;
+        orm.getPersonalData('users', userid, function(data) {
+            console.log(data);
+            res.render('employee/employee_edit_profile', { user: data });
+        });
+    });
+    app.get('/employee_edit_resume', function(req, res) {
+        // load the edit_profile file
+        res.render('employee/employee_edit_resume');
+    });
+
+
+    // =====================================
+    // Employeer ==============================
+    // =====================================
+    app.get('/employeer', function(req, res) {
+        // load the edit_profile file
+        res.render('employeer/employeer');
+    });
+    app.get('/employeer_search', function(req, res) {
+        // load the edit_profile file
+        res.render('employeer/employeer_search');
+    });
+    app.get('/employeer_edit_contact', function(req, res) {
+        // load the edit_profile file
+        res.render('employeer/employeer_edit_contact');
+    });
+    app.get('/employeer_add_test', function(req, res) {
+        // load the edit_profile file
+        res.render('employeer/employeer_add_test');
+    });
+    app.get('/employeer_edit_test', function(req, res) {
+        // load the edit_profile file
+        res.render('employeer/employeer_edit_test');
+    });
 
 };
 
