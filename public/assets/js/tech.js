9b52c1e082aec6b0cfb07eea6a45898a644301b8
FlyMoe <rrgarcia@gmail.com>
2016-07-18 19:07:44 -0400

    Added code for image upload

--- public/assets/js/tech.js
+++ public/assets/js/tech.js
@@ -1,38 +1,5 @@
 
 // Code used to upload a file.
-// $('#upload-input').on('change', function(){
-
-//   var files = $(this).get(0).files;
-
-//   if (files.length > 0){
-//     // create a FormData object which will be sent as the data payload in the
-//     // AJAX request
-//     var formData = new FormData();
-
-//     // loop through all the selected files and add them to the formData object
-//     for (var i = 0; i < files.length; i++) {
-//       var file = files[i];
-
-//       // add the files to formData object for the data payload
-//       formData.append('uploads[]', file, file.name);
-//     }
-
-//     $.ajax({
-//       url: '/upload',
-//       type: 'POST',
-//       data: formData,
-//       processData: false,
-//       contentType: false,
-//       success: function(data){
-//           console.log('upload successful!\n' + data);
-//       }
-//     });
-
-//   }
-// });
-
-
-// Code used to upload a file.
 $('#uploadButton').click(function(){
 
 //$('#upload-input').on('change', function(){
