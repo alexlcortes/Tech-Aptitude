feaa32ead5c8ea786cb7ed2b5648e74463ac46cf
FlyMoe <rrgarcia@gmail.com>
2016-07-21 23:03:27 -0400

    Fixed missing squiggly

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'root'
-
+        'password': 'family99'
+    },
 	'database': 'tech_db',
     'users_table': 'users'
 };
